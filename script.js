4413ceb09365e901ec8ccf749cfc3437f4b47801
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2022-01-20 16:31:30 +0800

    section-one

--- script.js
+++ script.js
@@ -6,27 +6,18 @@ const bodyElement = document.getElementById('body');
 navBar.addEventListener("click", function() {
     query.classList.toggle("query-active")
     navBar.classList.toggle('nav-bar-active')
-    bodyElement.classList.toggle('query-active')
+    bodyElement.classList.toggle('overlay')
 })
 
 window.addEventListener('scroll', function() {
     let scroll = window.scrollY;
-    if (scroll > 300) {
-        navContainer.style.position = 'fixed';
-        navContainer.style.backgroundColor = "rgba(255, 255, 255, 0.9)";
-        navContainer.style.marginTop = '10px';
-        navContainer.style.marginLeft = '1%';
-        navContainer.style.marginRight = 'auto';
-        navContainer.style.width = '98%'; 
-        navContainer.style.transition = ".5s";
+    if (scroll > 10) {
+        // navContainer.style.position = 'fixed';
+        navContainer.style.backgroundColor = "rgba(255, 255, 255, 0.9)"; 
+        navContainer.style.transition = ".3s";
     } else {
-        navContainer.style.position = 'static';
         navContainer.style.backgroundColor = "rgba(255, 255, 255, 0)"
-        navContainer.style.marginTop = '10px';
-        navContainer.style.marginLeft = 'auto';
-        navContainer.style.marginRight = 'auto';
-        navContainer.style.width = '98%';
-        navContainer.style.transition = ".5s";
+        navContainer.style.transition = ".3s";
     }
 
     console.log(scroll);
