214dde5c59c4865560367afabc74ea75f39cf951
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2022-01-21 23:22:06 +0800

    fixing footer

--- script.js
+++ script.js
@@ -5,16 +5,25 @@ const overLayer = document.querySelector('.overlayer');
 const imgChanger = document.querySelector('#img-changer');
 const imgChange = document.querySelector('#img-change');
 
-const imgs =['img/p10.jpg'];
+const titleContainerAbout = document.querySelector('.about');
+const horAbout = document.querySelector('.hor-about');
+const footerTextAbout = document.querySelector('.footer-text-about')
 
-// function mouseOut(){
-//     imgChange.src = "";
-// }
-// function mouseOver(e){
-//     // var imgChange = e.id;
-//     imgChange.src = imgs[0]
-//     console.log(imgChange);
-// }
+const titleContainerAddress = document.querySelector('.address');
+const horAddress = document.querySelector('.hor-address');
+const footerTextAddress = document.querySelector('.footer-text-address')
+
+
+
+titleContainerAbout.addEventListener('click', function(){
+    horAbout.classList.toggle('hor-hide');
+    footerTextAbout.classList.toggle('show-text');
+})
+// titleContainerAddress.addEventListener('click', function(){
+//     horAbout.classList.toggle('hor-hide');
+//     footerText.classList.toggle('show-text');
+// })
+
 
 navBar.addEventListener("click", function() {
     query.classList.toggle("query-active")
@@ -25,7 +34,6 @@ navBar.addEventListener("click", function() {
 window.addEventListener('scroll', function() {
     let scroll = window.scrollY;
     if (scroll > 10) {
-        // navContainer.style.position = 'fixed';
         navContainer.style.backgroundColor = "rgba(255, 255, 255, 0.9)"; 
         navContainer.style.transition = ".3s";
     } else {
