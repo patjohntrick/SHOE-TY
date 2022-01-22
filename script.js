9920debec7bb07fad2c86d25fb70acf398f49746
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2022-01-22 14:32:31 +0800

    finish

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const navBar = document.querySelector('.nav-bar');
-const query = document.querySelector('.query');
+var navBar = document.querySelector('.nav-bar');
+var query = document.querySelector('.query');
 const navContainer = document.querySelector('.nav-container');
-const overLayer = document.querySelector('.overlayer');
+var overLayer = document.querySelector('.overlayer');
 const imgChanger = document.querySelector('#img-changer');
 const imgChange = document.querySelector('#img-change');
 
@@ -13,16 +13,39 @@ const titleContainerAddress = document.querySelector('.address');
 const horAddress = document.querySelector('.hor-address');
 const footerTextAddress = document.querySelector('.footer-text-address')
 
+const titleContainerContact = document.querySelector('.contact');
+const horContact = document.querySelector('.hor-contact');
+const footerTextContact = document.querySelector('.footer-text-contact')
+
+const titleContainerInfo = document.querySelector('.info');
+const horInfo = document.querySelector('.hor-info');
+const footerTextInfo = document.querySelector('.footer-text-info')
+
+const titleContainerFollow = document.querySelector('.follow');
+const horFollow = document.querySelector('.hor-follow');
+const footerTextFollow = document.querySelector('.footer-text-follow')
 
 
 titleContainerAbout.addEventListener('click', function(){
-    horAbout.classList.toggle('hor-hide');
+    horAbout.classList.toggle('hor-hide-about');
     footerTextAbout.classList.toggle('show-text');
 })
-// titleContainerAddress.addEventListener('click', function(){
-//     horAbout.classList.toggle('hor-hide');
-//     footerText.classList.toggle('show-text');
-// })
+titleContainerAddress.addEventListener('click', function(){
+    horAddress.classList.toggle('hor-hide-address');
+    footerTextAddress.classList.toggle('show-text');
+})
+titleContainerContact.addEventListener('click', function(){
+    horContact.classList.toggle('hor-hide-contact');
+    footerTextContact.classList.toggle('show-text');
+})
+titleContainerInfo.addEventListener('click', function(){
+    horInfo.classList.toggle('hor-hide-info');
+    footerTextInfo.classList.toggle('show-text');
+})
+titleContainerFollow.addEventListener('click', function(){
+    horFollow.classList.toggle('hor-hide-follow');
+    footerTextFollow.classList.toggle('show-text');
+})
 
 
 navBar.addEventListener("click", function() {
@@ -32,14 +55,34 @@ navBar.addEventListener("click", function() {
 })
 
 window.addEventListener('scroll', function() {
-    let scroll = window.scrollY;
+    var scroll = window.scrollY;
+    console.log(scroll);
     if (scroll > 10) {
         navContainer.style.backgroundColor = "rgba(255, 255, 255, 0.9)"; 
         navContainer.style.transition = ".3s";
+        navContainer.style.boxShadow = '0 0 1px rgba(0, 0, 0, .4)';
     } else {
         navContainer.style.backgroundColor = "rgba(255, 255, 255, 0)"
         navContainer.style.transition = ".3s";
+        navContainer.style.boxShadow = '0 0 2px rgba(0, 0, 0, 0)';
     }
-
-    // console.log(scroll);
 });
+document.querySelector('.logo').addEventListener('click', function(){
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+})
+// const secondaryLink = document.querySelectorAll('.secondary-link').forEach(function(item){
+//     item.addEventListener('click', function(e){
+//         e.preventDefault();
+        
+//         navBar.classList.remove('nav-bar-active');
+//         query.classList.remove('query-active');
+//         overLayer.classList.remove('overlayed');
+//         // query.classList.toggle("query-active")
+//         // navBar.classList.toggle('nav-bar-active')
+//         // overLayer.classList.toggle('overlayed')
+//         })
+// })
